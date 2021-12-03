d4021eaf179ac510a528a7ba3f6192370dd0f497
X00T Best Kernels <xootkama@gmail.com>
2021-12-03 14:09:12 +0000

    Add Gov

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#0 SMP PREEMPT Fri Dec 3 13:38:48 UTC 2021"
+#define LINUX_COMPILE_BY "gitpod"
+#define LINUX_COMPILE_HOST "ws-1ccee9c6-2856-451a-aa13-7ff89f82d064"
+#define LINUX_COMPILER "clang version 10.0.0-4ubuntu1"
