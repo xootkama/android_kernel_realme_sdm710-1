a14b31ad771fbfda1ded44bb69a9311e41622f7e
ncsih <95001456+ncsih@users.noreply.github.com>
2021-12-12 19:34:14 +0530

    Update cpuidle.c

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -77,7 +77,8 @@ int cpuidle_play_dead(void)
 {
 	struct cpuidle_device *dev = __this_cpu_read(cpuidle_devices);
 	struct cpuidle_driver *drv = cpuidle_get_cpu_driver(dev);
-	int i;
+	int i; dead_state = -1;
+	int power_usage = INT_MAX
 
 	if (!drv)
 		return -ENODEV;
